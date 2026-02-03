3e8abe9f7ab6310f98c49786f4dc125c9aaacdbe
SameerJSRS <sameer@robotostudio.com>
2026-02-03 14:26:39 +0530

    config: update next.config and add cursor globals css

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
