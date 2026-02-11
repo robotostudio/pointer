195214a5b43d7280b8d2264527c46e64fe5b796b
toperobotostudio <tope@robotostudio.com>
2026-02-11 08:36:12 +0100

    chore:changed font weight from bold to regular

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -7,7 +7,7 @@ export async function GET(request: Request) {
   const title = url.searchParams.get("title") || "Pointer";
 
   const fontData = await readFile(
-    join(process.cwd(), "fonts/CursorGothic-Bold.woff2")
+    join(process.cwd(), "fonts/CursorGothic-Regular.woff2")
   );
 
   return new ImageResponse(
